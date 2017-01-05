302ae7357ecfbe36aae6f283650c9dc28720d76a
廉建杰 <lianjianjie@lianjianjiedeMacBook-Pro.local>
2017-01-05 11:21:54 +0800

    项目开始搭建

---
+++ Lian/Lian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lian
+//
+//  Created by 廉建杰 on 2017/1/5.
+//  Copyright © 2017年 lianjianjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lian/Lian/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lian
+//
+//  Created by 廉建杰 on 2017/1/5.
+//  Copyright © 2017年 lianjianjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
